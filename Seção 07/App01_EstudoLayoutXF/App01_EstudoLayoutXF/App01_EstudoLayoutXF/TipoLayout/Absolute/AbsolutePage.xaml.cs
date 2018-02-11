2991ff7f125caf99d17d91f24612d07a614755b4
Elias <Elias@DESKTOP-0HVK3TF>
2018-02-11 18:40:31 -0200

    Enviando nossos Estudos de Layout.

---
+++ Seção 07/App01_EstudoLayoutXF/App01_EstudoLayoutXF/App01_EstudoLayoutXF/TipoLayout/Absolute/AbsolutePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_EstudoLayoutXF.TipoLayout.Absolute
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AbsolutePage : ContentPage
+	{
+		public AbsolutePage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
