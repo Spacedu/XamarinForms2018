de14730ebfbd583108aa887033dde06dc3da3e0f
Elias <Elias@DESKTOP-0HVK3TF>
2018-02-13 16:38:56 -0200

    Enviando o projeto de Layout - App01

---
+++ Seção 07/App01_LayoutXF/App01_LayoutXF/App01_LayoutXF/Layouts/Absolute/AbsolutePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_LayoutXF.Layouts.Absolute
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AbsolutePage : ContentPage
+	{
+		public AbsolutePage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ Seção 07/App01_LayoutXF/App01_LayoutXF/App01_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_LayoutXF
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void GoPaginaStack(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Stack.StackPage());
+        }
+
+        private void GoPaginaGrid(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Grid.GridPage());
+        }
+
+        private void GoPaginaAbsolute(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+        }
+
+        private void GoPaginaRelative(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Relative.RelativePage());
+        }
+
+        private void GoPaginaScroll(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+        }
+
+    }
+}
