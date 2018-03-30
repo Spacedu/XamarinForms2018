3051fdcf4f521c8dc0575a229655c9cbf9dac736
Elias <Elias@DESKTOP-0HVK3TF>
2018-03-30 19:58:14 -0300

    App 2 Correações
    App 2 Correações

--- Seção 07/App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
+++ Seção 07/App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
@@ -19,13 +19,14 @@ namespace App02_Xamarin.Master
 
         private void GoPaginaPerfil1(object sender, EventArgs args)
         {
-            Detail = new Pages.Perfil1();
-            
+            Detail = new NavigationPage(new Pages.Perfil1());
+            IsPresented = false;
         }
 
         private void GoPaginaXamarin(object sender, EventArgs args)
         {
-            Detail = new Pages.Xamarin();
+            Detail = new NavigationPage(new Pages.Xamarin());
+            IsPresented = false;
         }
 
     }
