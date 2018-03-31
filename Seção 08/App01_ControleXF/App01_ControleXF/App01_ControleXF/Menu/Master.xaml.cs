9208763ea205c74c8621e2a846dd0c0db3c167f0
Elias <Elias@DESKTOP-0HVK3TF>
2018-03-31 20:01:35 -0300

    Correções provenientes das Demonstrações.

--- Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
+++ Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Menu/Master.xaml.cs
@@ -18,67 +18,83 @@ namespace App01_ControleXF.Menu
 		}
         private void GoActivityIndicatorPage(object sender, EventArgs args)
         {
-            Detail = new Controles.ActivityIndicatorPage();
+            Detail = new NavigationPage( new Controles.ActivityIndicatorPage() );
+            IsPresented = false;
         }
         private void GoProgressBarPage(object sender, EventArgs args)
         {
-            Detail = new Controles.ProgressBarPage();
+            Detail = new NavigationPage(new Controles.ProgressBarPage());
+            IsPresented = false;
         }
         private void GoBoxViewPage(object sender, EventArgs args)
         {
-            Detail = new Controles.BoxViewPage();
+            Detail = new NavigationPage(new Controles.BoxViewPage());
+            IsPresented = false;
         }
         private void GoLabelPage(object sender, EventArgs args)
         {
-            Detail = new Controles.LabelPage();
+            Detail = new NavigationPage(new Controles.LabelPage());
+            IsPresented = false;
         }
         private void GoButtonPage(object sender, EventArgs args)
         {
-            Detail = new Controles.ButtonPage();
+            Detail = new NavigationPage(new Controles.ButtonPage());
+            IsPresented = false;
         }
         private void GoEntryEditorPage(object sender, EventArgs args)
         {
-            Detail = new Controles.EntryEditorPage();
+            Detail = new NavigationPage(new Controles.EntryEditorPage());
+            IsPresented = false;
         }
         private void GoDatePickerPage(object sender, EventArgs args)
         {
-            Detail = new Controles.DatePickerPage();
+            Detail = new NavigationPage(new Controles.DatePickerPage());
+            IsPresented = false;
         }
         private void GoTimePickerPage(object sender, EventArgs args)
         {
-            Detail = new Controles.TimePickerPage();
+            Detail = new NavigationPage(new Controles.TimePickerPage());
+            IsPresented = false;
         }
         private void GoPickerPage(object sender, EventArgs args)
         {
-            Detail = new Controles.PickerPage();
+            Detail = new NavigationPage(new Controles.PickerPage());
+            IsPresented = false;
         }
         private void GoSearchBarPage(object sender, EventArgs args)
         {
-            Detail = new Controles.SearchBarPage();
+            Detail = new NavigationPage(new Controles.SearchBarPage());
+            IsPresented = false;
         }
         private void GoSliderStepperBarPage(object sender, EventArgs args)
         {
-            Detail = new Controles.SliderStepperPage();
+            Detail = new NavigationPage(new Controles.SliderStepperPage());
+            IsPresented = false;
         }
         private void GoSwitchPage(object sender, EventArgs args)
         {
-            Detail = new Controles.SwitchPage();
+            Detail = new NavigationPage(new Controles.SwitchPage());
+            IsPresented = false;
         }
         private void GoImagePage(object sender, EventArgs args)
         {
-            Detail = new Controles.ImagePage();
+            Detail = new NavigationPage(new Controles.ImagePage());
+            IsPresented = false;
         }
         private void GoListViewPage(object sender, EventArgs args)
         {
-            Detail = new Controles.ListViewPage();
+            Detail = new NavigationPage(new Controles.ListViewPage());
+            IsPresented = false;
         }
         private void GoTableViewPage(object sender, EventArgs args)
         {
-            Detail = new Controles.TableViewPage();
+            Detail = new NavigationPage(new Controles.TableViewPage());
+            IsPresented = false;
         }
         private void GoWebViewPage(object sender, EventArgs args)
         {
-            Detail = new Controles.WebViewPage();
+            Detail = new NavigationPage(new Controles.WebViewPage());
+            IsPresented = false;
         }
 
     }
