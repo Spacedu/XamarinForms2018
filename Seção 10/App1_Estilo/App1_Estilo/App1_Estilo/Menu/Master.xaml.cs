2a414aee8e98f34e9f9581b2ee51b56fa75c017f
Elias <Elias@DESKTOP-0HVK3TF>
2018-04-02 20:03:31 -0300

    Melhorias

--- Seção 10/App1_Estilo/App1_Estilo/App1_Estilo/Menu/Master.xaml.cs
+++ Seção 10/App1_Estilo/App1_Estilo/App1_Estilo/Menu/Master.xaml.cs
@@ -20,22 +20,27 @@ namespace App1_Estilo.Menu
         private void GoPagina1(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ImplicitStylePage());
+            IsPresented = false;
         }
         private void GoPagina2(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ExplicitStylePage());
+            IsPresented = false;
         }
         private void GoPagina3(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.GlobalStylePage());
+            IsPresented = false;
         }
         private void GoPagina4(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.InheritStylePage());
+            IsPresented = false;
         }
         private void GoPagina5(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.DynamicStylePage());
+            IsPresented = false;
         }
         
     }
