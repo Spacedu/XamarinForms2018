cb56a2a3fb6aa1af9fbc5d089f2bf45ead3b40c6
Elias <Elias@DESKTOP-0HVK3TF>
2018-04-02 16:28:18 -0300

    Enviando atualização Seção 9

--- Seção 09/App1_Cell/App1_Cell/App1_Cell/Menu/Master.xaml.cs
+++ Seção 09/App1_Cell/App1_Cell/App1_Cell/Menu/Master.xaml.cs
@@ -19,30 +19,37 @@ namespace App1_Cell.Menu
         public void GoPagina1(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.TextCellPage());
+            IsPresented = false;
         }
         public void GoPagina2(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ImageCellPage());
+            IsPresented = false;
         }
         public void GoPagina3(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.EntryCellPage());
+            IsPresented = false;
         }
         public void GoPagina4(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.SwitchCellPage());
+            IsPresented = false;
         }
         public void GoPagina5(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ViewCellPage());
+            IsPresented = false;
         }
         public void GoPagina6(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ListViewPage());
+            IsPresented = false;
         }
         public void GoPagina7(object sender, EventArgs args)
         {
             Detail = new NavigationPage(new Pagina.ListViewButtonPage());
+            IsPresented = false;
         }
     }
 }
